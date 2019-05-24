b4ce62585832ef59fad325d6df0cb578d3889080
zequeMorales <ezequielmorales5@icloud.com>
2019-05-24 11:35:10 -0300

    Initial commit

---
+++ src/paqueteven/Logueo.java
@@ -0,0 +1,107 @@
+package paqueteven;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.JInternalFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Logueo extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Logueo frame = new Logueo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Logueo() {
+		setTitle("LOGIN");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 394, 296);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.YELLOW);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblLogin.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setForeground(Color.BLACK);
+		lblLogin.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblLogin.setBackground(Color.YELLOW);
+		lblLogin.setBounds(145, 30, 122, 33);
+		contentPane.add(lblLogin);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(39, 80, 57, 15);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasenia = new JLabel("Contraseña");
+		lblContrasenia.setBounds(39, 133, 75, 15);
+		contentPane.add(lblContrasenia);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(145, 75, 122, 27);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(145, 127, 122, 27);
+		contentPane.add(txtPassword);
+		
+		JButton btnIngresar = new JButton("INGRESAR");
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				char [] clave= txtPassword.getPassword();
+				String claveFinal= new String (clave);
+				if(txtUsuario.getText().equals("admin") && claveFinal.equals("admin")){
+					dispose();
+					Principal p = new Principal();
+					p.setVisible(true);
+					
+				}else{
+					JOptionPane.showMessageDialog(null, "Usuario o Contraseña incorrectos", "ERROR", JOptionPane.ERROR_MESSAGE);
+					txtUsuario.setText("");
+					txtPassword.setText("");
+					txtUsuario.requestFocus();
+				}
+			}
+		});
+		btnIngresar.setBounds(145, 178, 122, 66);
+		contentPane.add(btnIngresar);
+	}
+}
