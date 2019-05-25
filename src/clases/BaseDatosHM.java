386efeef7518ec1fa4cf570cfe2fb96d808118e4
ZerooCooler <nolosonie1@gmail.com>
2019-05-25 00:00:12 -0300

    agregué la clase BaseDatosHM pero tengo un problema con la interfaz.

---
+++ src/clases/BaseDatosHM.java
@@ -0,0 +1,25 @@
+package clases;
+
+import java.util.HashMap;
+
+public class BaseDatosHM <K,T> implements IBaseDatos <T> {
+	private HashMap<K,T> listaMapa;
+
+	@Override
+	public void agregar(T o) {
+		
+		
+	}
+
+	@Override
+	public void borrar(T o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- src/clases/IBaseDatos.java
+++ src/clases/IBaseDatos.java
@@ -4,5 +4,6 @@ public interface IBaseDatos<T>{
 	public void agregar(T o);
 	public void borrar(T o);
 	public String listar();
+
 	
 }
