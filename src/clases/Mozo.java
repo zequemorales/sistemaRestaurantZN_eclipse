f65c51b057876de6c466ee6aee5fc6738dc734c2
nicocristobal <nicolas_emanuel@hotmail.com>
2019-06-07 11:36:09 -0300

    Javadoc de alguna clases
    producto, comida, bebida, persona, mozo , mesa

--- src/clases/Mozo.java
+++ src/clases/Mozo.java
@@ -1,23 +1,45 @@
 package clases;
-
+/**
+ * Clase Mozo
+ * 
+ * @param idMozo
+ * 				número de identificación del mozo
+ *
+ */
 public class Mozo extends Persona{
 	private int idMozo;
 
+	/**
+	 * Constructor
+	 * @param nombre
+	 * @param apellido
+	 * @param idMozo
+	 */
 	public Mozo(String nombre, String apellido, int idMozo) {
 		super(nombre, apellido);
 		setIdMozo(idMozo);
 	}
-
+	/**
+	 * Obtiene la identificación del mozo
+	 * @return int con la identificacion del mozo
+	 */
 	public int getIdMozo() {
 		return idMozo;
 	}
+	/**
+	 * Setea la identificación del mozo
+	 * @param idMozo
+	 * Recibe la identificación del mozo a setear
+	 */
 	public void setIdMozo(int idMozo) {
 		this.idMozo = idMozo;
 	}
-	
+	/**
+	 * Descripción del mozo
+	 * @return String de la descripción del mozo
+	 */
 	@Override
-	public String toString() {
-		
+	public String toString() {		
 		return super.toString() + "Id Mozo: " + getIdMozo();
 	}
 	
--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,32 +1,61 @@
 package clases;
-
+/**
+ * Clase Persona
+ * 
+ * @param nombre
+ * 				nombre de la persona
+ * @param apellido 
+ * 				apellido de la persona
+ */
 public class Persona {
 	private String nombre;
 	private String apellido;
 	
-		
+	/**
+	 * Constructor
+	 * @param nombre
+	 * @param apellido
+	 */
 	public Persona(String nombre, String apellido) {
 		setNombre(nombre);
 		setApellido(apellido);
 	}
-	
+	/**
+	 * Obtiene el nombre de la persona
+	 * @return
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * Setea el nombre de la persona
+	 * @param nombre
+	 * Recibe el nombre de la persona a setea
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * Obtiene el nombre de la persona
+	 * @return apellido
+	 */
 	public String getApellido() {
 		return apellido;
 	}
+	/**
+	 * Setea el apellido de la persona
+	 * @param apellido
+	 * Recibre el apellido de la perosna a setear
+	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
-	
- @Override
-public String toString() {
-	// TODO Apéndice de método generado automáticamente
-	return "Nombre: "+ getNombre()+" Apellido: "+ getApellido()+ " ";
-}
+	/**
+	 * Descripcion de la persona
+	 * @return String con la descripcion de la persona
+	 */
+	 @Override
+	public String toString() {
+		return "Nombre: "+ getNombre()+" Apellido: "+ getApellido()+ " ";
+	}
 }
